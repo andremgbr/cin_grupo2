e1ad27406416865f12db56b03ff5f7a5ac81a767
fabiohennr <marksfm@gmail.com>
2025-03-03 11:56:58 -0300

    Inserindo comentário no fim da lista produto

--- src/run.c
+++ src/run.c
@@ -8,7 +8,7 @@ Product products[SIZE_PRODUCTS] = {
   {"sabão", 1, 1.3}, 
   {"banana", 5, 3.3}, 
   {"vassoura", 0, 10},
-  {"", -1, -1} 
+  {"", -1, -1} // Produto sentinela (Indica fim da lista)
 };
 
   int exit = 0;
